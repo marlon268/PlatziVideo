99ce49af593c596ad236a58db20e3e55f3b3483a
marlon268 <varon.marlon83@gmail.com>
2021-05-20 09:21:04 -0500

    Proyecto finalizado del curso practico de React

---
+++ src/containers/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header.jsx";
+import Search from "../components/Search.jsx";
+import Categories from "../components/Categories.jsx";
+import Carousel from "../components/Carousel.jsx";
+import CarouselItem from "../components/CarouselItem.jsx";
+import Footer from "../components/Footer.jsx";
+import useInitialState from "../hooks/useInitialState.js";
+import "../assets/styles/App.scss";
+
+const API = "http://localhost:3000/initalState";
+
+const App = () => {
+  const initialState = useInitialState(API);
+  return (
+    <div className="App">
+      <Header />
+      <Search />
+      {initialState.mylist.length > 0 && (
+        <Categories title="Mi lista">
+          <Carousel>
+          {initialState.mylist.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+          </Carousel>
+        </Categories>
+      )}
+
+      <Categories title="Tendencias">
+        <Carousel>
+          {initialState.trends.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+
+      <Categories title="Originales de Platzi video">
+        <Carousel>
+          {initialState.originals.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+
+      <Footer />
+    </div>
+  );
+};
+export default App;
